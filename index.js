f3f8bf275bba516b9f72eed49c9e086c59fb847f
kalra-rahul <kalrarahul431@gmail.com>
2023-07-03 15:49:13 +0530

    change in index

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require('body-passsrser');
 const jsonParser = bodyParser.json();
 
 
 app.listen(3000,()=>{
-    console.log("app listen on 3000 port")
+    console.log("app listen on 3000sss port")
 })
 
 
